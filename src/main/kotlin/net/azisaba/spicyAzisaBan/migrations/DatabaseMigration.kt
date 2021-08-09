b5fa1887a1eea98073079d721cc0aefb43bd1ad5
acrylic-style <me@acrylicstyle.xyz>
2021-08-10 02:42:51 +0900

    show the time took to complete operation

--- src/main/kotlin/net/azisaba/spicyAzisaBan/migrations/DatabaseMigration.kt
+++ src/main/kotlin/net/azisaba/spicyAzisaBan/migrations/DatabaseMigration.kt
@@ -8,6 +8,7 @@ interface DatabaseMigration {
         private val migrations = listOf<DatabaseMigration>()
 
         fun run(): Promise<Unit> = Promise.create { context ->
+            val start = System.currentTimeMillis()
             val initialVersion = SpicyAzisaBan.instance.settings.getDatabaseVersion().complete()
             SpicyAzisaBan.instance.logger.info("Running database migrations (current database version: $initialVersion)")
             SpicyAzisaBan.instance.logger.info("${migrations.size} migrations loaded.")
@@ -15,17 +16,20 @@ interface DatabaseMigration {
                 val version = SpicyAzisaBan.instance.settings.getDatabaseVersion().complete()
                 if (migration.targetDatabaseVersion == version) {
                     SpicyAzisaBan.instance.logger.info("Migrating '${migration.name}' (database version $version)")
+                    val sectionStart = System.currentTimeMillis()
                     try {
                         migration.execute()
                     } catch (e: Throwable) {
                         SpicyAzisaBan.instance.logger.severe("Error migrating '${migration.name}' (database version $version)")
                         throw e
                     }
-                    SpicyAzisaBan.instance.logger.info("Migrated '${migration.name}' (database version $version)")
+                    val sectionTime = System.currentTimeMillis() - sectionStart
+                    SpicyAzisaBan.instance.logger.info("Migrated '${migration.name}' (database version $version, took ${sectionTime}ms)")
                 }
             }
             val version = SpicyAzisaBan.instance.settings.getDatabaseVersion().complete()
-            SpicyAzisaBan.instance.logger.info("Completed database migrations (current database version: $version)")
+            val time = System.currentTimeMillis() - start
+            SpicyAzisaBan.instance.logger.info("Completed database migrations (current database version: $version, took ${time}ms)")
             SpicyAzisaBan.instance.settings.setDatabaseVersion(version)
             context.resolve()
         }
