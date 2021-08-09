1e31b77d113a217eff418575663e9e4675ce3734
acrylic-style <me@acrylicstyle.xyz>
2021-08-10 02:40:22 +0900

    don't use promise here

--- src/main/kotlin/net/azisaba/spicyAzisaBan/migrations/DatabaseMigration.kt
+++ src/main/kotlin/net/azisaba/spicyAzisaBan/migrations/DatabaseMigration.kt
@@ -1,14 +1,13 @@
 package net.azisaba.spicyAzisaBan.migrations
 
 import net.azisaba.spicyAzisaBan.SpicyAzisaBan
-import util.kt.promise.rewrite.catch
 import util.promise.rewrite.Promise
 
 interface DatabaseMigration {
     companion object {
         private val migrations = listOf<DatabaseMigration>()
 
-        fun run(): Promise<Unit> = Promise.create {
+        fun run(): Promise<Unit> = Promise.create { context ->
             val initialVersion = SpicyAzisaBan.instance.settings.getDatabaseVersion().complete()
             SpicyAzisaBan.instance.logger.info("Running database migrations (current database version: $initialVersion)")
             SpicyAzisaBan.instance.logger.info("${migrations.size} migrations loaded.")
@@ -16,19 +15,19 @@ interface DatabaseMigration {
                 val version = SpicyAzisaBan.instance.settings.getDatabaseVersion().complete()
                 if (migration.targetDatabaseVersion == version) {
                     SpicyAzisaBan.instance.logger.info("Migrating '${migration.name}' (database version $version)")
-                    migration.execute()
-                        .catch {
-                            SpicyAzisaBan.instance.logger.severe("Error migrating '${migration.name}' (database version $version)")
-                            throw it
-                        }
-                        .complete()
+                    try {
+                        migration.execute()
+                    } catch (e: Throwable) {
+                        SpicyAzisaBan.instance.logger.severe("Error migrating '${migration.name}' (database version $version)")
+                        throw e
+                    }
                     SpicyAzisaBan.instance.logger.info("Migrated '${migration.name}' (database version $version)")
                 }
             }
             val version = SpicyAzisaBan.instance.settings.getDatabaseVersion().complete()
             SpicyAzisaBan.instance.logger.info("Completed database migrations (current database version: $version)")
             SpicyAzisaBan.instance.settings.setDatabaseVersion(version)
-            it.resolve()
+            context.resolve()
         }
     }
 
@@ -36,5 +35,5 @@ interface DatabaseMigration {
         get() = "Migration for database version $targetDatabaseVersion"
     val targetDatabaseVersion: Int
 
-    fun execute(): Promise<Unit>
+    fun execute()
 }
