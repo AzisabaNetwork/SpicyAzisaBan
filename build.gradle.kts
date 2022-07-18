a5db9338b68a2cabca87b0747bc4f0447648dafa
acrylic-style <me@acrylicstyle.xyz>
2022-07-18 16:43:43 +0900

    duplicate artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,7 +109,6 @@ subprojects {
         publications {
             create<MavenPublication>("mavenJava") {
                 from(components["java"])
-                artifact(tasks.getByName("sourcesJar"))
             }
         }
     }
