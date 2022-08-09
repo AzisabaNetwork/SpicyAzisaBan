a2b6381dee84fbd4296403feff73af607d23ec21
acrylic-style <me@acrylicstyle.xyz>
2022-08-09 13:52:16 +0900

    Fix time= missing from suggestions of /gtempmute

--- common/src/main/kotlin/net/azisaba/spicyAzisaBan/commands/GlobalTempMuteCommand.kt
+++ common/src/main/kotlin/net/azisaba/spicyAzisaBan/commands/GlobalTempMuteCommand.kt
@@ -21,7 +21,7 @@ import util.kt.promise.rewrite.catch
 object GlobalTempMuteCommand: Command() {
     override val name = "${SABConfig.prefix}gtempmute"
     override val permission = PunishmentType.TEMP_MUTE.perm
-    private val availableArguments = listOf(listOf("player="), listOf("reason=\"\""), listOf("server="), listOf("--all", "-a"), listOf("--force", "-f"))
+    private val availableArguments = listOf(listOf("player="), listOf("reason=\"\""), listOf("server="), listOf("time="), listOf("--all", "-a"), listOf("--force", "-f"))
 
     override fun execute(actor: Actor, args: Array<String>) {
         if (!actor.hasPermission(PunishmentType.TEMP_MUTE.perm)) {
